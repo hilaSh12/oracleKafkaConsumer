46d8f0bb9e939b010fc96e8b438f8310c0ade53d
hilaSh12 <54236118+hilaSh12@users.noreply.github.com>
2020-03-31 13:44:31 +0300

    Update ggConsumerMain.py

--- consumer/ggConsumerMain.py
+++ consumer/ggConsumerMain.py
@@ -1,4 +1,5 @@
 import ggConsumer
+import s3Utils
 import json
 
 # something in my computer doesn't allow me to set environment variables so here i use regular variables instead
@@ -6,16 +7,19 @@ kafkaHost = '40.114.93.145:9094'
 s3AccKey = 'public_key'
 s3SecretKey = 'public_secret'
 s3Host = 'http://bdservices-minio.idf-cts.com:9000'
-entities = '{"ora-gg-events": ["hila", "or/events", "True", "False"], "ora-gg-reports": ["hila", "or/reports", "True", "True"]}'
+entities = '{"ora-gg-events": {"destBucket": "hila", "directoryInsideBucket": "or/events", "isPartitionByYear": "True", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-reports": {"destBucket": "hila", "directoryInsideBucket": "or/reports", "isPartitionByYear": "True", "isPartitionByMonth": "True"},' \
+           ' "ora-gg-eventsextra": {"destBucket": "hila", "directoryInsideBucket": "or/eventsextra", "isPartitionByYear": "True", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-reportsextra": {"destBucket": "hila", "directoryInsideBucket": "or/reportsextra", "isPartitionByYear": "True", "isPartitionByMonth": "True"}, ' \
+           ' "ora-gg-journals": {"destBucket": "hila", "directoryInsideBucket": "or/journals", "isPartitionByYear": "False", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-journalsextra": {"destBucket": "hila", "directoryInsideBucket": "or/journalsextra", "isPartitionByYear": "False", "isPartitionByMonth": "False"}, ' \
+           ' "ora-gg-hila": {"destBucket": "hila", "directoryInsideBucket": "or/hila", "isPartitionByYear": "True", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-shalisman": {"destBucket": "hila", "directoryInsideBucket": "or/shalisman", "isPartitionByYear": "True", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-hilashalisman": {"destBucket": "hila", "directoryInsideBucket": "or/hilashalisman", "isPartitionByYear": "True", "isPartitionByMonth": "False"},' \
+           ' "ora-gg-hilash": {"destBucket": "hila", "directoryInsideBucket": "or/hilash", "isPartitionByYear": "True", "isPartitionByMonth": "False"}}'
 
-# kafkaHost = os.environ.get('kafkaHost')
-# s3AccKey = os.environ.get('s3AccKey')
-# s3SecretKey = os.environ.get('s3SecretKey')
-# s3Host = os.environ.get('s3Host')
-# entities = os.environ.get('entities')
+linkTopicToS3Location = json.loads(entities)
 
-dictEntities = json.loads(entities)
-
-rawData = ggConsumer.getDeltaFromKafka(kafkaHost)
-s3Conn = ggConsumer.createS3Connection(s3AccKey, s3SecretKey, s3Host)
-ggConsumer.useConsumedData(rawData, s3Conn, dictEntities)
+kafkaConsumer = ggConsumer.getDeltaFromKafka(kafkaHost)
+s3Conn = s3Utils.createS3Connection(s3AccKey, s3SecretKey, s3Host)
+ggConsumer.useConsumedData(kafkaConsumer, s3Conn, linkTopicToS3Location)
